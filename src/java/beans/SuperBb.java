897aea45cc57af8fd85461d3403bbb20072d17bb
am <am@W0268>
2016-10-07 10:12:06 +0900

    変数と getter/setter を作成

--- src/java/beans/SuperBb.java
+++ src/java/beans/SuperBb.java
@@ -6,11 +6,160 @@
 package beans;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  *
  * @author am
  */
 public class SuperBb implements Serializable{
+    // Bb 用スーパークラス
+
+    // UserDetailBb と AddDataBb 用変数
+    private Integer user_id;
+    private String name_kanji;
+    private String name_kana;
+    private String tel;
+    private String email;
+    private String age;
+    private Boolean sex;
+    private String password;
+    private String register;
+    private Integer theater_info_id;
+    private Integer room_num;
+    private String movietitle;
+    private String showdate;
+    private String showtime;
+    private Integer seat_id;
     
+    //SeatBb 用変数
+    private Integer theater_id;
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName_kanji() {
+        return name_kanji;
+    }
+
+    public void setName_kanji(String name_kanji) {
+        this.name_kanji = name_kanji;
+    }
+
+    public String getName_kana() {
+        return name_kana;
+    }
+
+    public void setName_kana(String name_kana) {
+        this.name_kana = name_kana;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public Boolean getSex() {
+        return sex;
+    }
+
+    public void setSex(Boolean sex) {
+        this.sex = sex;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRegister() {
+        return register;
+    }
+
+    public void setRegister(String register) {
+        this.register = register;
+    }
+
+    public Integer getTheater_info_id() {
+        return theater_info_id;
+    }
+
+    public void setTheater_info_id(Integer theater_info_id) {
+        this.theater_info_id = theater_info_id;
+    }
+
+    public Integer getRoom_num() {
+        return room_num;
+    }
+
+    public void setRoom_num(Integer room_num) {
+        this.room_num = room_num;
+    }
+
+    public String getMovietitle() {
+        return movietitle;
+    }
+
+    public void setMovietitle(String movietitle) {
+        this.movietitle = movietitle;
+    }
+
+    public String getShowdate() {
+        return showdate;
+    }
+
+    public void setShowdate(String showdate) {
+        this.showdate = showdate;
+    }
+
+    public String getShowtime() {
+        return showtime;
+    }
+
+    public void setShowtime(String showtime) {
+        this.showtime = showtime;
+    }
+
+    public Integer getSeat_id() {
+        return seat_id;
+    }
+
+    public void setSeat_id(Integer seat_id) {
+        this.seat_id = seat_id;
+    }
+
+    public Integer getTheater_id() {
+        return theater_id;
+    }
+
+    public void setTheater_id(Integer theater_id) {
+        this.theater_id = theater_id;
+    }
 }
